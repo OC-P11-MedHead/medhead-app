ba41a2f0dcafeecc17d7c6800f3e6efa9e5a595e
Yoann VALERO <yoann.valero7@gmail.com>
2024-02-28 17:29:24 +0100

    tests e2e with protractor cmd

--- medhead-front/protractor.conf.js
+++ medhead-front/protractor.conf.js
@@ -1,12 +1,10 @@
 exports.config = {
-    seleniumAddress: 'http://localhost:4444/wd/hub',
     capabilities: {
         browserName: 'chromium', // Vous pouvez également utiliser 'firefox', 'safari', etc.
         chromeOptions: {
-            args: ['--no-sandbox', '--disable-gpu'],
+            args: ['--headless', '--disable-gpu', '--window-size=1920x1080'],
         },
     },
     specs: ['./e2e/*.js'],
       // Adresse de votre application Angular en cours d'exécution
-    baseUrl: 'http://localhost:4200/',
 }
