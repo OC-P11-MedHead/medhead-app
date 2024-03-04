f972861e68e63b0aba75fd7a02f8fd4efa251fbc
YoannValero <yoann.valero7@gmail.com>
2024-03-04 10:53:08 +0100

    test pipeline with less second wait

--- medhead-front/e2e/bookBed.js
+++ medhead-front/e2e/bookBed.js
@@ -7,17 +7,17 @@ async function testBookBed() {
         await driver.get('http://localhost:4200/')
         await driver.findElement(By.id('modal')).click()
         // await driver.wait(until.titleIs('MedheadFront'), 3000)
-        await driver.sleep(1000)
+        await driver.sleep(500)
         await driver.findElement(By.id('select-hospital')).click()
         
-        await driver.sleep(1000)
+        await driver.sleep(500)
         await driver.findElement(By.xpath("//span[contains(text(),'The Mall, London SW1A 2WH, Royaume-Uni')]")).click()
-        await driver.sleep(1000)
+        await driver.sleep(500)
 
         await driver.findElement(By.id('select-speciality')).click()
-        await driver.sleep(1000)
+        await driver.sleep(500)
         await driver.findElement(By.xpath("//span[contains(text(),'Cardiologie')]")).click()
-        await driver.sleep(1000)
+        await driver.sleep(500)
         console.log("Tests e2e : success")
     } catch {
         console.log("Tests e2e : error")
